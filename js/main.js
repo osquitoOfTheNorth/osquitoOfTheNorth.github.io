f9b458401107bb1fd169e61328678e2ab36b4c10
Oscar Menjivar <oscar.m@ecobee.com>
2020-02-08 18:24:58 -0500

    update resume and work exp.

--- js/main.js
+++ js/main.js
@@ -10,14 +10,15 @@ var shellTextArray = ["Oscar = new Oscar()",
                       "Oscar.Interests",
                       "[\"code\", \"cooking\", \"music\", \"travel\", \"food\" ]",
                       "Oscar.WorkExperience",
-                      "[{ <br> <p id=\"indent\"> company:\"SapientRazorfish\"<br>role: \"Associate Mobile Applications Developer\"<br>startDate:\"July 2016\"<br>endDate: null </p>}," +
+                      "[{ <br> <p id=\"indent\"> company:\"Ecobee\"<br>role: \"Android Developer\"<br>startDate:\"Nov 2018\"<br>endDate: null </p>}," +
+                        "{ <br> <p id=\"indent\"> company:\"SapientRazorfish\"<br>role: \"Associate Mobile Applications Developer\"<br>startDate:\"July 2016\"<br>endDate: Nov 2018 </p>}," +
                         "{ <br> <p id=\"indent\"> company:\"RBC Capital Markets\"<br>role: \"Technical Systems Analyst\"<br>startDate:\"May 2014\"<br>endDate: Sept 2015 </p>}];",
                       "Oscar.Resume",
-                      "<a class=\"link\" href=\"./Oscar Menjivar Resume 2015 v3.docx\">\"oscarmenjivar.doc\"</a>",
+                      "<a class=\"link\" href=\"./Oscar Menjivar Resume_2020.docx\">\"oscarmenjivar.doc\"</a>",
                       "Oscar.email",
                       "\"oscarmenj02@gmail.com\"",
                       "Oscar.socialMedia",
-                      "[\"<a class=\"link\" href=\"https://github.com/osquitoOfTheNorth\">GitHub</a>\", \"<a class=\"link\" href='http://www.linkedin.com/profile/view?id=217534388&amp;trk=nav_responsive_tab_profile_pic'>LinkedIn</a>\", \"<a class=\"link\" href='http://oscarandthoughts.postach.io/' >Blog</a>\" ]"
+                      "[\"<a class=\"link\" href=\"https://github.com/osquitoOfTheNorth\">GitHub</a>\", \"<a class=\"link\" href='http://www.linkedin.com/profile/view?id=217534388&amp;trk=nav_responsive_tab_profile_pic'>LinkedIn</a>\"]"
                       ];
 var shellLineIdentifiers = ["1Declaration", "2Declaration", "3Declaration", "4Declaration", "5Declaration", "6Declaration", "7Declaration",
                             "8Declaration", "9Declaration", "10Declaration", "11Declaration", "12Declaration", "13Declaration","14Declaration","15Declaration",
@@ -91,7 +92,7 @@ jQuery(document).ready(function ($) {
                     }
 
     });
-    
+
 });
 
 function ToggleDisplay(){
@@ -101,7 +102,7 @@ function ToggleDisplay(){
         duration:1500,
         complete:function(){
             $("#information").css("margin", "0 auto");
-            $("#information").css("display","inherit");    
+            $("#information").css("display","inherit");
             $("#information").animate({
             width: "50%",
             height: "100%",
@@ -266,4 +267,3 @@ function resetAndGoToNextLine(IsTyped){
     $("#information").append("<div id=\"cursor\" ></div>");
 
 }
-
